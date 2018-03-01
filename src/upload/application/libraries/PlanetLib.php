2af8ca3a3ce54eaf6cf490f64067e0300869646c
Christophe TAPPRET <christophe.tappret@orange.com>
2018-03-01 13:16:20 +0100

    Fixing #4 and #5 Here is the line to fix the DB update xgp_planets set planet_b_hangar_id='' where planet_b_hangar_id=0;

--- src/upload/application/libraries/PlanetLib.php
+++ src/upload/application/libraries/PlanetLib.php
@@ -143,7 +143,7 @@ class PlanetLib extends XGPCore
                     'planet_deuterium' => BUILD_DEUTERIUM,
                     'planet_deuterium_perhour' => FunctionsLib::readConfig('deuterium_basic_income'),
                     'planet_b_building_id' => '0',
-                    'planet_b_hangar_id' => '0'
+                    'planet_b_hangar_id' => ''
                 ]
             );
 
@@ -213,7 +213,7 @@ class PlanetLib extends XGPCore
                     'planet_temp_min' => $min_temp == 0 ? $temp['min'] : $min_temp,
                     'planet_temp_max' => $max_temp == 0 ? $temp['max'] : $max_temp,
                     'planet_b_building_id' => '0',
-                    'planet_b_hangar_id' => '0'
+                    'planet_b_hangar_id' => ''
                 ]
             );
         
